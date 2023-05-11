4c91c4ffb01681db6cb1f51577fe5ac5d25f058d
Logan May <MAY16011454@btc.ac.uk>
2023-05-11 14:42:25 +0100

    Added Room and Equipment Admin to Dashboard

---
+++ Areas/Admin/Controllers/EquipmentController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FitnessApplication.Models;
+using ITS_System.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FitnessApplication.Areas.Admin.Views
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+
+    public class EquipmentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EquipmentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Equipment
+        public async Task<IActionResult> Index()
+        {
+              return _context.Equipments != null ? 
+                          View(await _context.Equipments.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Equipments'  is null.");
+        }
+
+        // GET: Admin/Equipment/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Equipments == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipment);
+        }
+
+        // GET: Admin/Equipment/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Equipment/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description")] Equipment equipment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(equipment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(equipment);
+        }
+
+        // GET: Admin/Equipment/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Equipments == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipments.FindAsync(id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+            return View(equipment);
+        }
+
+        // POST: Admin/Equipment/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Equipment equipment)
+        {
+            if (id != equipment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(equipment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EquipmentExists(equipment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(equipment);
+        }
+
+        // GET: Admin/Equipment/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Equipments == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipment);
+        }
+
+        // POST: Admin/Equipment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Equipments == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Equipments'  is null.");
+            }
+            var equipment = await _context.Equipments.FindAsync(id);
+            if (equipment != null)
+            {
+                _context.Equipments.Remove(equipment);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EquipmentExists(int id)
+        {
+          return (_context.Equipments?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
