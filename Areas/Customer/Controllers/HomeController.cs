a291e01364e575593b910e2972ba4e642183efa6
Logan May <MAY16011454@btc.ac.uk>
2023-05-25 07:14:22 +0100

    Added a customer dashboard

---
+++ Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ITS_System.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize(Roles = "Customer")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
