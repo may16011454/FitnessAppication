9c13b856ba33e16a65c211a269e3481673086431
Logan May <MAY16011454@btc.ac.uk>
2023-05-25 10:54:57 +0100

    Added Customer View Class

---
+++ Areas/Customer/Controllers/ViewClassController.cs
@@ -0,0 +1,25 @@
+﻿using ITS_System.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessApplication.Areas.Customer.Controllers
+{
+    public class ViewClassController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ViewClassController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [Area("Customer")]
+        [Authorize(Roles = "Customer")]
+        public IActionResult Index()
+        {
+            var applicationDbContext = _context.Schedule.ToList();
+            return View(applicationDbContext);
+        }
+    }
+}
